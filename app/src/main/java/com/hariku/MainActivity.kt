42e726762223ae8ec7d0666f7977095426350710
samuelalfito <samuel.deanova@gmail.com>
2025-10-20 21:58:54 +0700

    chore: architecture

--- app/src/main/java/com/hariku/MainActivity.kt
+++ app/src/main/java/com/hariku/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.hariku.ui.theme.HariKuTheme
+import com.hariku.core.ui.theme.HariKuTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/hariku/core/ui/theme/Color.kt
+++ app/src/main/java/com/hariku/core/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.hariku.ui.theme
+package com.hariku.core.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
