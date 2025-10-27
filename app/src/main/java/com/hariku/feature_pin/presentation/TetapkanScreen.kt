dfa9f6368040925c0ef9a8950315cf4b34254fc4
Amet <sunowar66@gmail.com>
2025-10-27 14:44:16 +0700

    ganti dikit

--- app/src/main/java/com/hariku/feature_pin/presentation/TetapkanScreen.kt
+++ app/src/main/java/com/hariku/feature_pin/presentation/TetapkanScreen.kt
@@ -33,7 +33,7 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp~
+import androidx.compose.ui.unit.sp
 import com.hariku.R
 
 @Composable
