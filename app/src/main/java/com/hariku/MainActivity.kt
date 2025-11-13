5f779070d2a02d53ed6ad85fe81d8d080071de2c
VlynXx <rendiadipradananh@gmail.com>
2025-11-13 07:41:29 +0700

    feat: Add MeditationSongCompletedScreen

--- app/src/main/java/com/hariku/MainActivity.kt
+++ app/src/main/java/com/hariku/MainActivity.kt
@@ -6,42 +6,31 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.hariku.core.ui.theme.HariKuTheme
+import com.hariku.feature_meditation.presentation.MeditationSongCompletedScreen
+
+// Pastikan kode di MainActivity terlihat persis seperti ini:
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             HariKuTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // HANYA ini yang harus dipanggil.
+                    MeditationSongCompletedScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HariKuTheme {
-        Greeting("Android")
     }
 }
