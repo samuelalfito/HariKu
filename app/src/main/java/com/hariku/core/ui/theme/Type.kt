5fe3c178a9a20b97deae943a7bcb3aa52b9cf899
samuelalfito <samuel.deanova@gmail.com>
2025-11-28 14:57:54 +0700

    fix(core): fixing font family issue

--- app/src/main/java/com/hariku/core/ui/theme/Type.kt
+++ app/src/main/java/com/hariku/core/ui/theme/Type.kt
@@ -2,14 +2,25 @@ package com.hariku.core.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.hariku.R
+
+private val Rubik = FontFamily(
+    Font(R.font.rubik_light, FontWeight.Light),
+    Font(R.font.rubik_regular, FontWeight.Normal),
+    Font(R.font.rubik_medium, FontWeight.Medium),
+    Font(R.font.rubik_semibold, FontWeight.SemiBold),
+    Font(R.font.rubik_bold, FontWeight.Bold),
+    Font(R.font.rubik_black, FontWeight.Black)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Rubik,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
